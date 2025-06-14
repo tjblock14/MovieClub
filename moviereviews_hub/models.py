4c29072e3f6c92dc3a8d922899656fab79d4a8f3
tjblock14 <trevorblock19@gmail.com>
2025-06-14 13:24:29 -0500

    genre fix

--- moviereviews_hub/models.py
+++ moviereviews_hub/models.py
@@ -13,12 +13,9 @@ class Movie(models.Model):
     director = ArrayField(models.CharField(max_length = 100))
 
     starring_actors = models.CharField(max_length=200, null=True, blank=True)  # keep temp
-    actors = ArrayField(
-        models.CharField(max_length=100),
-        default=list,       # ✅ ← Add this
-    )
-    
-    genres = ArrayField(models.CharField(max_length = 150))
+    actors = ArrayField(models.CharField(max_length=100), default = list)      # default = list to avoid an issue I was having where
+                                                                               # every character was separated by a comma
+    genres = ArrayField(models.CharField(max_length = 150), default = list)
     slug = models.SlugField(max_length = 200, unique = True, blank = True) # Automatically assigns a slug value to the title
 
     def save(self, *args, **kwargs):
