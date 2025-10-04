557fa7de4b60e5fbb3cd06981abf02e3c180e76d
tjblock14 <trevorblock19@gmail.com>
2025-10-04 10:46:41 -0500

    not sure what all this is, committing to main

--- movieclub_backend/moviereviews_hub/urls.py
+++ movieclub_backend/moviereviews_hub/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MovieViewSet, ReviewViewSet, couple_specific_reviews  # couple specific reviews needed for function based view
+from .views import club_average_ratings # Average club rating for all movies
 
 router = DefaultRouter()
 router.register(r'movies', MovieViewSet, basename = 'movie')
@@ -10,5 +11,8 @@ urlpatterns = [
     path('', include(router.urls)),
 
     # This path will return all movies in the database along with all the reviews by a specific couple based on the slug (tt, mn, sb)
-    path('couple_reviews/<slug:couple_slug>/', couple_specific_reviews)
+    path('couple_reviews/<slug:couple_slug>/', couple_specific_reviews),
+
+    # This path returns every movie with its club average rating
+    path('club_average/', club_average_ratings, name = 'club_average')
 ]
